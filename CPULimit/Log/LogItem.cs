6aa3655dc370aaa659199f1419bb3521c6158278
Unreal-System <31071269+Unreal-System@users.noreply.github.com>
2018-03-06 08:41:28 +0800

    Add files via upload

---
+++ CPULimit/Log/LogItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CPULimit.Log
+{
+    internal class LogItem
+    {
+        /// <summary>
+        /// 时间
+        /// </summary>
+        internal DateTime Date { get; set; }
+
+        /// <summary>
+        /// 日志级别
+        /// </summary>
+        internal LogType LogType { get; set; }
+
+        /// <summary>
+        /// 错误信息
+        /// </summary>
+        internal String LogContent { get; set; }
+
+        internal LogItem(LogType logType, String logContent)
+        {
+            this.Date = DateTime.Now;
+            this.LogType = logType;
+            this.LogContent = logContent;
+        }
+
+        public override String ToString()
+        {
+            return $"[{Date}] @ [{LogType}]>>{LogContent}";
+        }
+    }
+}
